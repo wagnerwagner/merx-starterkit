352a3b096f435099e8ef9fdca5feeed1aa5dc3de
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2022-02-23 17:24:38 +0100

    Updates /site/snippets/footer.php

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
   <div class="text">
     <p><strong>Merx Starterkit Demo Shop</strong></p>
-    <p><a href="https://github.com/wagnerwagner/merx-starterkit" rel="noopener">Code on GitHub</a>
+    <p><a href="https://github.com/wagnerwagner/merx-starterkit" rel="noopener">Source Code on GitHub</a></p>
   </div>
 </footer>
